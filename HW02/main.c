34b06557c62995263f0df7155ce01b2abdfcecdd
Ivan Voronov <i1ravenov@gmail.com>
2025-12-23 20:16:09 +0400

    Remove .idea from repo

--- HW02/main.c
+++ HW02/main.c
@@ -32,7 +32,6 @@ int main(int argc, char *argv[]) {
     char url[256];
 
     snprintf(url, sizeof(url), "https://wttr.in/%s?format=j1", argv[1]);
-    printf("%s\n", url);
     CURL *curl;
     CURLcode res;
 
@@ -55,10 +54,45 @@ int main(int argc, char *argv[]) {
     curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
 
     res = curl_easy_perform(curl);
-    printf("%s\n", chunk.buf);
     // Check for errors
     if(res != CURLE_OK)
         fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+
+    cJSON *root = cJSON_Parse(chunk.buf);
+    if (!root) {
+        printf("JSON parse error\n");
+        return 1;
+    }
+
+    // 1. Получаем поле current_condition (это массив)
+    cJSON *current_condition = cJSON_GetObjectItem(root, "current_condition");
+    if (!cJSON_IsArray(current_condition)) {
+        printf("current_condition is not array\n");
+        cJSON_Delete(root);
+        return 1;
+    }
+
+    // 2. Берём первый элемент массива
+    cJSON *current = cJSON_GetArrayItem(current_condition, 0);
+    if (!cJSON_IsObject(current)) {
+        printf("current_condition[0] is not object\n");
+        cJSON_Delete(root);
+        return 1;
+    }
+
+    cJSON *temp_C = cJSON_GetObjectItem(current, "temp_C");
+    cJSON *date = cJSON_GetObjectItem(current, "localObsDateTime");
+    cJSON *pressure = cJSON_GetObjectItem(current, "pressure");
+    cJSON *winddir = cJSON_GetObjectItem(current, "winddir16Point");
+    cJSON *windspeed = cJSON_GetObjectItem(current, "windspeedKmph");
+    printf("The weather in %s for date: %s\n", argv[1], date->valuestring);
+    printf("Temperature       : %s°C\n", temp_C->valuestring);
+    printf("Pressure          : %s\n", pressure->valuestring);
+    printf("Direction of wind : %s\n", winddir->valuestring);
+    printf("Wind speed in km/h: %s\n", windspeed->valuestring);
+    cJSON_Delete(root);
+
 
     // Clean up
     curl_easy_cleanup(curl);
